0545f40065b55f8c383f8f67679e102bb2a7e5be
Warren <warren@ucsd.edu>
2024-05-24 20:54:34 -0700

    Basic page-list item search function implemented: no filters yet

--- src/scripts/list.js
+++ src/scripts/list.js
@@ -5,15 +5,15 @@ document.addEventListener("DOMContentLoaded", init());
 
 function init() {
   displayList(journalList);
+  setUpSearch();
 }
 
-function displayList (journalList){
-    //Iterate through list and append them to HTML
-    journalList.forEach((item) => {
-        createListItem(item);
-    });
+function displayList(list) {
+  //Iterate through list and append them to HTML
+  list.forEach((item) => {
+    createListItem(item);
+  });
 }
-
 
 function createListItem(item) {
   //Get the essential elements
@@ -28,7 +28,7 @@ function createListItem(item) {
   const details = document.createElement("div");
   details.style.fontSize = "small";
 
-//   Get and set timestamp
+  //Get and set timestamp
   const timestamp = new Date(parseInt(item.timestamp)).toLocaleString();
   const timestampText = document.createElement("div");
   timestampText.textContent = `Timestamp: ${timestamp}`;
@@ -43,7 +43,7 @@ function createListItem(item) {
     tagElement.textContent = tag;
     tagElement.className = "tag";
     tagElement.onclick = () => {
-    // Event triggered when clicking into tag. Future feature for filter search
+      // Event triggered when clicking into tag. Future feature for filter search
     };
     tagsContainer.appendChild(tagElement);
     tagsContainer.appendChild(document.createTextNode(" ")); // Add space between tags
@@ -58,7 +58,7 @@ function createListItem(item) {
   deleteButton.className = "delete-button";
   deleteButton.style.display = "none";
 
-// EventListener: When clicking delete, delete from page and LocalStorage
+  // EventListener: When clicking delete, delete from page and LocalStorage
   deleteButton.onclick = () => {
     listItem.remove();
     deleteJournal(item.timestamp);
@@ -72,23 +72,57 @@ function createListItem(item) {
     deleteButton.style.display = "none";
   };
 
-//   Append the entire list item into the list
+  //   Append the entire list item into the list
   itemList.appendChild(listItem);
 }
 
 function getJournalList() {
-    if (!localStorage.getItem("GarlicNotes")) {
-        return [];
-    } else {
-        return JSON.parse(localStorage.getItem("GarlicNotes"));
-    }
+  if (!localStorage.getItem("GarlicNotes")) {
+    return [];
+  } else {
+    return JSON.parse(localStorage.getItem("GarlicNotes"));
+  }
 }
 
 function deleteJournal(timestamp) {
-    journalList = journalList.filter(entry => entry.timestamp != timestamp);
-    saveJournal(journalList);
+  journalList = journalList.filter(entry => entry.timestamp != timestamp);
+  saveJournal(journalList);
 }
 
 function saveJournal(journalList) {
-    localStorage.setItem("GarlicNotes", JSON.stringify(journalList));
+  localStorage.setItem("GarlicNotes", JSON.stringify(journalList));
+}
+
+function getMatchingEntries(list, query) {
+  let matchingEntriesByTitle = [];
+  let matchingEntriesByContent = [];
+  query = query.toLowerCase();
+  // Prioritize entries matching on title before matching on content.
+  list.forEach((entry) => {
+    if (entry.title.toLowerCase().includes(query)) {
+      matchingEntriesByTitle.push(entry);
+    } else if (getTextFromDelta(entry.delta).toLowerCase().includes(query)) {
+      matchingEntriesByContent.push(entry);
+    }
+  });
+  return matchingEntriesByTitle.concat(matchingEntriesByContent);
+}
+
+function getTextFromDelta(delta) {
+  // Include each string in insert operations within a Quill Delta
+  let text = '';
+  delta.ops.forEach((op) => {
+    text += op.insert;
+  });
+  return text;
+}
+
+function setUpSearch() {
+  const searchBar = document.getElementById("search-bar");
+  // EventListener: After typing, filter items to those matching search
+  searchBar.oninput = () => {
+    const itemList = document.getElementById("item-list");
+    itemList.replaceChildren(); // Empty item list
+    displayList(getMatchingEntries(journalList, searchBar.value));
+  };
 }
