bde321cfa229b1055682f6f5c03afc99d7a46915
Warren <warren@ucsd.edu>
2024-05-30 07:43:13 -0700

    start unit tests for src/scripts/list.js on src/tests/unitTests.test.js for unit testing diligence, and include jest jsdom environment on package.json for ability for unit tests to work

--- src/tests/unitTests.test.js
+++ src/tests/unitTests.test.js
@@ -1,3 +1,51 @@
+/**
+ * @jest-environment jsdom
+ */
+import { getTextFromDelta } from "../scripts/list";
+
 test("adds 1 + 2 to equal 3", () => {
   expect(1 + 2).toBe(3);
+});
+
+test("getTextFromDelta to correctly grab 'getTextFromDelta to\n correctly\n grab journal text\n'", () => {
+  let delta = {"ops": [
+    {
+      "attributes": {
+        "italic": true
+      },
+      "insert": "getTe"
+    },
+    {
+      "attributes": {
+        "italic": true,
+        "bold": true
+      },
+      "insert": "xtFrom"
+    },
+    {
+      "attributes": {
+        "italic": true
+      },
+      "insert": "Delta to"
+    },
+    {
+      "insert": "\n correctly"
+    },
+    {
+      "attributes": {
+        "list": "bullet"
+      },
+      "insert": "\n"
+    },
+    {
+      "insert": " grab journal text"
+    },
+    {
+      "attributes": {
+        "list": "ordered"
+      },
+      "insert": "\n"
+    }
+  ]};
+  expect(getTextFromDelta(delta)).toStrictEqual("getTextFromDelta to\n correctly\n grab journal text\n");
 });
