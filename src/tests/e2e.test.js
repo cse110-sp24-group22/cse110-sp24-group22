89c5c9804e07de76c22f24d6482f25986c6a816a
Aaron-石雪岩 <43869308+Aaron3963@users.noreply.github.com>
2024-06-07 15:53:36 -0700

    Fixed with testing

--- src/tests/e2e.test.js
+++ src/tests/e2e.test.js
@@ -1,9 +1,13 @@
-describe('Basic user flow for Website', () => {
-    // First, visit the lab 8 website
-    beforeAll(async () => {
-      await page.goto('http://127.0.0.1:5501/src/html/list.html');
-    });
-    test("dummy", () => {
-      
-    });
+describe("Basic user flow for Website", () => {
+  // First, visit the lab 8 website
+  beforeAll(async () => {
+    await page.goto("http://127.0.0.1:5500/src/html/list.html");
+  });
+
+  it("Initial Check for 0 journal", async () => {
+    // Get all journal entries on the page
+    const journalEntries = await page.$$("item-list");
+    // Check that the number of journal entries is 0
+    expect(journalEntries.length).toBe(0);
+  });
 });
