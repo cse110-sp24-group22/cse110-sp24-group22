0187ba38c646f5c51c4a63de98e6928514a9e821
Aaron3963 <43869308+Aaron3963@users.noreply.github.com>
2024-06-07 17:01:03 -0700

    Coverage test pipeline created (#100)
    * Testing coverage report
    
    * Temporally make workflow trigger on push
    
    * Update e2e.yml so it specifies the location of coverage report
    
    * Update e2e.yml with correct naming
    
    * Update e2e.yml changes
    
    * Add coverage source file
    
    * Change to lcov for test coverage
    
    * Update ignore so it shows in workflow
    
    * Fixed with testing
    
    * Minor update
    
    * Use unit test for code coverage
    
    * Revert back to default trigger
    
    ---------
    
    Co-authored-by: SatvikSriram <79161050+SatvikS7@users.noreply.github.com>

--- .github/workflows/jest.e2e.cjs
+++ .github/workflows/jest.e2e.cjs
@@ -1,5 +1,11 @@
 module.exports = {
     preset: 'jest-puppeteer',
+    collectCoverage: true,
     testMatch: ['**/tests/e2e.test.js'],
-    rootDir: "../../"
+    rootDir: "../../",
+    collectCoverageFrom: [
+      '**/src/**/*.js', // Adjust this pattern to match the files for which you want to collect coverage
+      '!**/tests/**/*.js',
+    ],
+    coverageReporters: ['json', 'lcov', 'text', 'clover'],
   };
